c478a5e97bc6088cc2ac2d2cd7768e3702833f8f
Isaac Mango <bigmangohead@gmail.com>
2025-07-04 03:19:25 -0500

    Add Watch Pedestal Item

---
+++ src/main/java/net/bigmangohead/neutronsim/common/WatchPedestalItem.java
@@ -0,0 +1,19 @@
+package net.bigmangohead.neutronsim.common;
+
+import net.bigmangohead.neutronsim.NeutronSim;
+import net.minecraft.item.Item;
+
+public class WatchPedestalItem extends Item {
+    protected String name;
+
+    public WatchPedestalItem() {
+        this.name = "watch_pedestal_item";
+        setRegistryName(name);
+    }
+
+    public void registerItemModel() {
+        NeutronSim.proxy.registerItemRenderer(this, 0, name);
+    }
+
+
+}
